16b4da94334af4083daa396cd85c9b172f4aaba0
Nathasya Christien <nathasya.abenita@gmail.com>
2026-07-18 14:20:29 +0200

    script to convert mask shapefile to nc file

---
+++ scripts/data_preprocess/mask_polygon_to_nc.py
@@ -0,0 +1,25 @@
+import geopandas as gpd
+import xarray as xr
+import numpy as np
+from shapely.geometry import Point
+
+# Extract polygon from geojson
+gdf = gpd.read_file('./data/shp/Aceh.geojson')
+polygon = gdf.geometry.iloc[0] 
+
+# Read grid example
+ds = xr.open_dataset("./data/final_exp/mask_SRF.nc")
+lon = ds["xlon"] # lon[iy, jx]
+lat = ds["xlat"] # lat[iy, jx]
+
+# Update mask array
+mask = np.zeros(lon.shape, dtype=bool)
+
+for i in range(lon.shape[0]):
+    for j in range(lon.shape[1]):
+        mask[i, j] = polygon.contains(Point(lon[i, j], lat[i, j]))
+
+# Finalized new NC file
+ds_ = ds.copy()
+ds_.mask.values = mask
+ds_.to_netcdf("mask_aceh.nc")
