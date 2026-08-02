aa450ec025380bd4acd057f4581f4ec319d34566
Nathasya Christien <nathasya.abenita@gmail.com>
2026-08-02 22:00:37 +0200

    update updraft script

---
+++ scripts/plots/pgw/final/updraft.py
@@ -0,0 +1,17 @@
+from mod_pgw import PGW
+
+if __name__ == '__main__':
+    # Create spatial map
+    pgw = PGW(filename='wa_max.nc', val_min=0, val_max=3,
+                        unit=r"Maximum Updraft (m s$^{-1}$)",
+                        var_name='wa', time_start="2025-11-25", time_end="2025-11-26",
+                        time_stat='max', ens_stat='mean')
+    # pgw.plot_map(outfile=r'./figs/compare/finals/map_wa.png')
+
+    # Create distribution change graph
+    pgw = PGW(filename='wa_max.nc', val_min=0, val_max=1,
+                            unit=r"Maximum Updraft (m s$^{-1}$)",
+                            var_name='wa', time_start="2025-11-25", time_end="2025-11-26",
+                            time_stat=None, ens_stat='mean')
+    pgw.plot_dist_change(change_val_min=0.5, change_val_max=5,
+                         change_min=0, change_max=17.5, outfile=r'./figs/compare/finals/dist_wa')
